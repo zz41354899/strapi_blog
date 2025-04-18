307deebecd36b5eb32a0d87d76f3f29375961be2
zz41354899 <zz41354899@gmail.com>
2025-04-18 14:38:16 +0800

    我們修改postgresql

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = 'postgres';
 
   const connections = {
     mysql: {
@@ -23,21 +23,15 @@ export default ({ env }) => {
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
+      // postgresql://root:4t5W0oJ98vRpN7k6csAmxX2OlT1ej3uV@hkg1.clusters.zeabur.com:30478/zeabur
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        host: "hkg1.clusters.zeabur.com",
+        port:30478,
+        database: "zeabur",
+        user: "root",
+        password: "4t5W0oJ98vRpN7k6csAmxX2OlT1ej3uV",
+        ssl: false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
